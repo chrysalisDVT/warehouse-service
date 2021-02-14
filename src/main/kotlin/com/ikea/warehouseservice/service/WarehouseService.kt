965cfd15ea065c2868f6b4af087b5b43a15f8435
DharmVT <10697203+chrysalisDVT@users.noreply.github.com>
2021-02-14 20:40:27 +0100

    Update WarehouseService.kt
    Update: Inventory stock information service

--- src/main/kotlin/com/ikea/warehouseservice/service/WarehouseService.kt
+++ src/main/kotlin/com/ikea/warehouseservice/service/WarehouseService.kt
@@ -12,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.dao.NonTransientDataAccessException
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
+import reactor.core.publisher.Mono
+import java.util.function.Consumer
 import java.util.stream.Collectors
 
 /**
@@ -52,28 +54,47 @@ class WarehouseService @Autowired constructor(
      * Update the inventory with the latest stock details based on the product exchange
      */
     @Transactional
-    fun updateInventoryByArtIds(articlesVo: WarehouseVO?, isUpdate: Boolean) {
+    fun updateInventoryByArtIds(articlesVo: WarehouseVO?, isUpdate: Boolean): Mono<WarehouseVO> {
         log.info("Updating the inventory based on the product transaction")
-        //Checking if the article is available
         try {
+
+            //Checking if the article is available
             articlesVo?.let {
                 val articleStockMap = articlesVo.warehouseInventory?.stream()?.collect(Collectors.toMap(WarehouseEntity::artId, WarehouseEntity::stock))
-                var warehouseDetails: MutableList<WarehouseEntity>? = articleStockMap?.let { articles -> warehouseRepository.findByArticleByArtIds(articles.keys) }
-                //Updating the warehouse inventory with the updated stock, if updated stock is negative the we cannot sell that product and exception is thrown
-                warehouseDetails
-                        ?.forEach { inventory ->
-                            when (isUpdate) {
-                                true -> inventory.stock?.minus(articleStockMap?.get(inventory.artId)!!)
+                //Updated implementation
+                val availableArticleStock = articlesVo.warehouseInventory?.parallelStream()?.map(WarehouseEntity::artId)?.let {
+                    warehouseRepository.findByArticleByArtIds(it.collect(Collectors.toSet()))
+                }
+                val outOfStockArticle = availableArticleStock?.parallelStream()?.filter { it.stock!! < articleStockMap?.get(it.artId)!! }?.collect(Collectors.toList())
+                var inventoryConsumer: (wareHouseEntity: WarehouseEntity) -> Unit =
+                        if (isUpdate) {
+                        //Consumer body
+                            {
+                                if (outOfStockArticle.isNullOrEmpty()) {
+                                    print("Test inventory deduction: $it")
+                                    it.stock?.minus(articleStockMap?.get(it.artId)!!)
+                                            ?.let { articleStock ->
+                                                warehouseRepository.updateArticleByArticle(it.artId, if (articleStock >= 0) articleStock else 0)
+                                            }
+                                }
+                            }
+                        //Consumer body end
+                        } else {
+                        //Consumer body
+                            {
+                                print("Test inventory addition: $it")
+                                it.stock?.plus(articleStockMap?.get(it.artId)!!)
                                         ?.let { articleStock ->
-                                            warehouseRepository.updateArticleByArticle(inventory.artId, if (articleStock > 0) articleStock else 0)
-                                        }
-                                false -> inventory.stock?.plus(articleStockMap?.get(inventory.artId)!!)
-                                        ?.let { articleStock ->
-                                            warehouseRepository.updateArticleByArticle(inventory.artId, if (articleStock > 0) articleStock else 0)
+                                            warehouseRepository.updateArticleByArticle(it.artId, if (articleStock >= 0) articleStock else 0)
                                         }
                             }
 
+                        //Consumer body end
                         }
+                //Updating the warehouse inventory with the updated stock, if updated stock is negative the we cannot sell that product and exception is thrown
+                availableArticleStock
+                        ?.forEach(inventoryConsumer)
+                return Mono.just(WarehouseVO(operationStatus = outOfStockArticle.isNullOrEmpty()))
             } ?: run {
                 throw WarehouseServiceException(EXCEPTION_CODE.BAD_REQUEST, "Exception occurred while updating inventory")
             }
